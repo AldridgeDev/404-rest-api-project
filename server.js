c7c65c4fc2b849b9a4093f8d91ac8b2c2ced45c5
Antonio Aldridge <aldridge.developer@gmail.com>
2023-02-23 16:29:49 -0500

    finished

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
@@ -14,4 +14,4 @@ app.use(express.json())
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
-app.listen(3000, () => console.log('This server has started'))
+app.listen(3000, () => console.log('Server Started'))
